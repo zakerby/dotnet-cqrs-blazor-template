8e3b6ad53422230d293407e82f73fcd304cff625
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2025-03-07 21:06:32 +0100

    Keep configuring MudBlazor

--- src/Core.Frontend/Program.cs
+++ src/Core.Frontend/Program.cs
@@ -1,10 +1,13 @@
 using Core.Frontend.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/") });
 
@@ -17,6 +20,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
