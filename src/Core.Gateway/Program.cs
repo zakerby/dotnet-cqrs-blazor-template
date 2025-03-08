904af42bda700064165520aff2d00738eb353c71
Nicolas Kerdelhué <nkerdelhue@gmail.com>
2025-03-08 22:01:33 +0100

    Update ocelot.json configuration to map the Core.Service

--- src/Core.Gateway/Program.cs
+++ src/Core.Gateway/Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+namespace OcelotBasic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s => {
+                s.AddOcelot();
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                //add your logging
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            })
+            .Build()
+            .Run();
+        }
+    }
+}
